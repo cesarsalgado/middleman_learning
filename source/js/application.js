e4bbeae2bd279074732fe8183337152acf941255
Cesar Salgado <cesarsvs@gmail.com>
2013-08-02 14:36:25 -0300

    Added js function to calculate age.
    I may use it in the future.

--- source/js/application.js
+++ source/js/application.js
@@ -8,6 +8,12 @@
 
     var $window = $(window)
 
+    // source: http://stackoverflow.com/a/15555947
+    function calcAge(dateString) {
+      var birthday = +new Date(dateString);
+      return ~~((Date.now() - birthday) / (31557600000));
+    }
+
     // side bar
     setTimeout(function () {
       $('.bs-docs-sidenav').affix({
