15f34e13c1a99aeb657fcaa58dc9db1577b4a47b
Cesar Salgado <cesarsvs@gmail.com>
2013-08-14 18:24:18 -0300

    Added new page called contact.
    I also added some jquery to the applications.js to handle the
    exposition of my email in the contacts page.
    
    I also deleted two links from the top navbar and added one link to
    contacts. I deleted the link to apps and more.

--- source/js/application.js
+++ source/js/application.js
@@ -3,3 +3,19 @@ function calcAge(dateString) {
   var birthday = +new Date(dateString);
   return ~~((Date.now() - birthday) / (31557600000));
 }
+
+$( "#generate_email_form" ).submit(function(e) {
+  e.preventDefault();
+  var firstname = $("#generate_email_form").find('input[placeholder="firstname"]').val();
+  var one = $("#generate_email_form").find('input[placeholder="1"]').val();
+  var two = $("#generate_email_form").find('input[placeholder="2"]').val();
+  var three = $("#generate_email_form").find('input[placeholder="3"]').val();
+
+  if (firstname == "cesar" && one == "s" && two == "v" && three == "s") {
+    $( "#generated_succesful_email" ).text(firstname + one + two + three + "@gmail.com");
+    $( "#generated_wrong_email" ).text("");
+  } else {
+    $( "#generated_succesful_email" ).text("");
+    $( "#generated_wrong_email" ).text("You made some mistake. Please, read again the rule and try again.");
+  }
+});
