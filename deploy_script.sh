a5bfeb67e892ec5b85c36c962f2d6d8ee9a50919
Cesar Salgado <cesarsvs@gmail.com>
2013-11-06 19:52:32 -0200

    Created file to semi-automate the deployment.
    Created file to semi-automate the deployment process to Github Pages.

---
+++ deploy_script.sh
@@ -0,0 +1,11 @@
+bundle exec middleman build
+git checkout gh-pages
+
+# Remove all files in derectory except the .git... local_resources and build
+find . -maxdepth 1 -not -regex "\(.*/\..*\)\|\.\|\./local_resources\|\./build" | xargs git rm -rf
+mv build/* .
+
+# do the part below manually to ensure everything is all right.`
+#git add --all
+#git commit
+#git push origin gh-pages
